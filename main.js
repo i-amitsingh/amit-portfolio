6c4f7fe9f69a2aa449899bc40994be7a8ac9cfb1
Amit Singh <106347663+i-amitsingh@users.noreply.github.com>
2023-08-19 03:01:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "Web Developer",
+      "UI/UX Designer",
+      "Web Designer",
+      "Competitive Coder",
+      "Graphic Designer",
+      
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 100,
+  });
--- script.js
+++ script.js
@@ -4,8 +4,12 @@ sideNav.style.right = "-250px";
 menuBtn.onclick = function () {
   if (sideNav.style.right == "-250px") {
     sideNav.style.right = 0;
+    menuBtn.src = "images/menubtncross.png";
+    
   } else {
     sideNav.style.right = "-250px";
+    menuBtn.src = "images/menu.png";
+    menuBtn.style.mixBlendMode = multiply;
   }
 };
 var scroll = new SmoothScroll('a[href*="#"]' ,{
